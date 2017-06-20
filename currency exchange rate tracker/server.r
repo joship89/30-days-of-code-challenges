2ce9f6292aec90a3777bfefc10b55a18e95b7086
joship89 <pjoshi@apptopia.com>
2017-06-19 20:37:52 -0400

    - code cleanup, variable name modifications

--- currency exchange rate tracker/server.r
+++ currency exchange rate tracker/server.r
@@ -2,38 +2,36 @@ library(shiny)
 library(data.table)
 library(ggplot2)
 library(quantmod)
+library(BiocInstaller)
+devtools::install_github("joshuaulrich/quantmod@144_getFX")
+setwd("~/github/data-science/projects/extras/useful-shiny-applications")
 
-
-# Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
 
   output$summary <- renderText({ 
     "Track convergence and divergence of 30, 60 and 90 day moving averages of currency conversion rates." 
     })
   
-  
-  output$distPlot <- renderPlot({
+  output$maPlot <- renderPlot({
     
-    # devtools::install_github("joshuaulrich/quantmod@144_getFX")
-    
-    Currencies <- 'USD/INR'
-    
+    currencies <- 'USD/INR'
     to.date <- Sys.Date() 
     from.date <-  to.date - 149 
       
-    getFX(Currencies,
+    # get conversion rates for the time interval
+    
+    getFX(currencies,
           from = from.date, to = to.date,
           env = .GlobalEnv,
           verbose = FALSE,
           warning = TRUE,
           auto.assign = TRUE)
     
-    
     USDINR <- setDT(as.data.frame(USDINR), keep.rownames = TRUE)[]
     colnames(USDINR) <- c("Date","Rate")
     USDINR <- USDINR %>% arrange(desc(Date))
     
-    #calculate rolling means. need vector with rates for last 60 days
+    # Calculate rolling means. need vector with rates for last 60 days
     USDINR.abs <- data.frame(USDINR$Date[1:30], as.data.frame(USDINR$Rate)[1:30,])
     USDINR.30.ma <- data.frame(USDINR$Date[1:30], as.data.frame(rollmean(USDINR$Rate, 30))[1:30,])
     USDINR.60.ma <- data.frame(USDINR$Date[1:30], rollmean(USDINR$Rate, 60)[1:30]) 
@@ -42,6 +40,7 @@ shinyServer(function(input, output) {
     colnames(moving.averages) <- c("Date", "30 Day", "60 Day", "90 Day","Current")
     
     
+    # line plot of moving averages
     
     ma.plot <- ggplot(data=moving.averages, 
       aes(x=Date, y=`30 Day`, group=1)) + 
@@ -55,6 +54,7 @@ shinyServer(function(input, output) {
       theme_bw() + 
       theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       theme(axis.title=element_text(face="bold", size="12", color="darkgreen"))
+    
     plot(ma.plot)
     
 
