77af7195bdfdff6924bfe7e50888ab3aa1ba1390
joshipras <joshi.pras@husky.neu.edu>
2017-07-17 22:38:33 -0400

    added Day0, Day1 challenges

---
+++ 1 - Data Types.py
@@ -0,0 +1,22 @@
+
+# Declare second integer, double, and String variables.
+
+i_2 = int(input())
+d_2 = float(input())
+s_2 = raw_input()
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+print i + i_2
+
+# Print the sum of the double variables on a new line.
+
+print d + d_2
+
+# Concatenate and print the String variables on a new line
+
+print "%s%s" %(s,  s_2)
+
+# The 's' variable above should be printed first.
